6fb7b4735965754b8da1baf15524c7912d924198
Tilden Winston <tildenwinston@gmail.com>
2019-09-28 19:50:32 -0400

    Added main page
    added basic page

--- HowRichDoYouLook/src/app/app.module.ts
+++ HowRichDoYouLook/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
 
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
