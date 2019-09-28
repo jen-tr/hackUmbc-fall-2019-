de317d2980aa28ef959af3963e170b7adf9d4e06
Tilden Winston <tildenwinston@gmail.com>
2019-09-28 16:41:36 -0400

    Main page init
    Main page initialized
    Material theme added

--- HowRichDoYouLook/src/app/app.module.ts
+++ HowRichDoYouLook/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
