1f9e9905d2bd9e2ae2470d22f66db316678b3771
Tilden Winston <tildenwinston@gmail.com>
2019-09-29 05:28:09 -0400

    Broken, then fixed...

--- backend/gimmeprices.py
+++ backend/gimmeprices.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from yahoo_fin import stock_info as si
 from pymongo import MongoClient
+
 splash = '''
  ____  _             _      ____        _        -\r\n/ ___|| |_ ___   ___| | __ |  _ \\  __ _| |_ __ _| |__   __ _ ___  ___ \r\n\\___ \\| __/ _ \\ / __| |/ / | | | |/ _` | __/ _` | '_ \\ / _` / __|/ _ \\\r\n ___) | || (_) | (__|   <  | |_| | (_| | || (_| | |_) | (_| \\__ \\  __/\r\n|____/ \\__\\___/ \\___|_|\\_\\ |____/ \\__,_|\\__\\__,_|_.__/ \\__,_|___/\\___|\r\n\r\n _   _           _       _            \r\n| | | |_ __   __| | __ _| |_ ___ _ __ \r\n| | | | '_ \\ / _` |/ _` | __/ _ \\ '__|\r\n| |_| | |_) | (_| | (_| | ||  __/ |   \r\n \\___/| .__/ \\__,_|\\__,_|\\__\\___|_|   \r\n
 '''
@@ -9,11 +10,11 @@ print(splash)
 print("\n=========================\nWith love from Zaine\n=========================\n")
 
 print("[~] Connecting to DB")
-client=MongoClient("172.17.0.2",27017) #After spinning up the docker container, cat /etc/hosts to find it's IP. fill that shit in here
-db=client['stox_database'] #gimme a databasse to query against
+client = MongoClient("35.230.173.155", 14532)  # After spinning up the docker container, cat /etc/hosts to find it's IP. fill that shit in here
+db = client['stox_database']  # gimme a databasse to query against
 print("[*] Connection OK!")
 
-stox = {'aapl','nvda','x','amzn','amd','ibm'} #add some more symbols here for fun and profit
+stox = {'aapl', 'nvda', 'x', 'amzn', 'amd', 'ibm'}  # add some more symbols here for fun and profit
 print("[~] Starting Iteration")
 
 while True:
@@ -33,4 +34,5 @@ while True:
 			print("[*] Post OK for "+i)
 		except Exception as e:
 			print("[E] Error on "+i+". Symbol may not exist")
+			print("\t"+str(e))
 			print("\t"+str(e))
