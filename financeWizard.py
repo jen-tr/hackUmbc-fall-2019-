8cd195a8e48983bb6e18eb7a76b5c28fd5de25f9
jen-tr <55922622+jen-tr@users.noreply.github.com>
2019-09-28 21:42:45 -0400

    Update financeWizard.py

--- financeWizard.py
+++ financeWizard.py
@@ -1,1 +1,35 @@
 
+
+
+
+import sys
+
+
+
+if __name__ == "__main__":
+
+    budget = raw_input("What is your budget?")
+
+    fobject = file("data.txt","rw")
+    stocks = []
+    maxVal = None
+    maxIndex = 0
+
+    for count,line in enumerate(fobject.readlines()):
+        currline = line.split()
+        stocks.append({"name":currline[0],"price":currline[1]})
+
+        if float(currline[1]) <= float(budget) and (maxVal is None):
+            maxVal = currline[1]
+            maxIndex = count
+
+        elif float(currline[1]) <= float(budget) and (float(currline[1]) < maxVal):
+            maxVal = currline[1]
+            maxIndex = count
+
+    if maxVal == None:
+        print("You can't afford any stocks.")
+
+    else:
+        print("With your budget, you can purchase stock: " + stocks[maxIndex]["name"] + " at the price of $" + stocks[maxIndex]["price"])
+
